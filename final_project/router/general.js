486cf3c08067eedefd0c4e9f4fe67464bb4b4e69
Silpakorn Dachanuwattana <art.silpakorn@gmail.com>
2024-11-06 01:02:31 -0500

    Get all books based on title. Get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,17 +34,23 @@ public_users.get('/author/:author',function (req, res) {
     return res.status(300).json({message:"${author} is not found"});
   }
 });
-
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  for (const isbn in books){
+    if(books[isbn].title == title){
+      res.send(books[isbn]);
+    }
+    return res.status(300).json({message:"${title} is not found"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  res.send(book[isbn].reviews)
 });
 
 module.exports.general = public_users;
